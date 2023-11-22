7f6c88a9cd2aa6e70a7733bc2507b3485f154184
anshmidt <anshmidt_ilya@list.ru>
2023-11-22 22:21:09 +0100

    Added sending SMS from MainActivity

--- app/src/main/java/com/anshmidt/smswidget/MainActivity.kt
+++ app/src/main/java/com/anshmidt/smswidget/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.anshmidt.smswidget
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.telephony.SmsManager
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,11 +15,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.datastore.preferences.core.preferencesOf
 import androidx.glance.appwidget.ExperimentalGlanceRemoteViewsApi
 import com.anshmidt.smswidget.ui.theme.AppTheme
+import android.Manifest
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 class MainActivity : ComponentActivity() {
+
+    private val SEND_SMS_PERMISSION_REQUEST_CODE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,7 +41,76 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        // Check for SMS permission before sending
+        if (checkPermission()) {
+            // Permission is already granted, send SMS
+            sendSMS()
+        } else {
+            // Request SMS permission
+            requestPermission()
+        }
     }
+
+
+    private fun checkPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.SEND_SMS
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.SEND_SMS),
+            SEND_SMS_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            SEND_SMS_PERMISSION_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission granted, send SMS
+                    sendSMS()
+                } else {
+                    // Permission denied, show a toast or handle accordingly
+                    Toast.makeText(
+                        this,
+                        "SMS permission denied. Cannot send SMS.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                return
+            }
+            else -> {
+                // Ignore other requests for now
+            }
+        }
+    }
+
+    private fun sendSMS() {
+        val phoneNumber = "1234567890" // Replace with the recipient's phone number
+        val message = "Hello, this is a test message: ${
+            LocalDateTime.now()}"
+
+        try {
+            val smsManager = SmsManager.getDefault()
+            smsManager.sendTextMessage(phoneNumber, null, message, null, null)
+            Toast.makeText(this, "SMS sent successfully", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Toast.makeText(this, "Failed to send SMS", Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
+        }
+    }
+
 }
 
 @Composable
